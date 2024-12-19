1dfad35f7b83dbc94e63f4b49ce26cc70d4d476c
xucaijun <xucaijun@kuaishou.com>
2024-12-19 21:34:01 +0800

    change key

--- MATH-NEW/config.py
+++ MATH-NEW/config.py
@@ -1,5 +1,5 @@
 # config.py
 
-OPENAI_API_KEY = 'my key'
+OPENAI_API_KEY = None
 TRAINING_DATA_PATH = './MATH-NEW/Thesis-for-LLM/MATH-NEW/train'  # 数据库的根路径
 OUTPUT_PATH = './MATH-NEW/answers'       # 存储答案的路径
