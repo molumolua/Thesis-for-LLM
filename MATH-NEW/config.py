30c78534288036056c579894acd09a302d6f4eb2
xucaijun <xucaijun@kuaishou.com>
2024-12-19 21:32:55 +0800

    change key

--- MATH-NEW/config.py
+++ MATH-NEW/config.py
@@ -1,5 +1,5 @@
 # config.py
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'my key'
 TRAINING_DATA_PATH = './MATH-NEW/Thesis-for-LLM/MATH-NEW/train'  # 数据库的根路径
 OUTPUT_PATH = './MATH-NEW/answers'       # 存储答案的路径
